0f6e39cbb3ad968a10a6da8d6fe3777ba7f12290
2043965148@qq.com <2043965148@qq.com>
2024-08-16 01:46:10 +0800

    初始化仓库

---
+++ src/main/java/com/brian/process/ProcessControlApplication.java
@@ -0,0 +1,13 @@
+package com.brian.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/process/ProcessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
